f37389e893c4c87c26818c7b78246fd2df06180c
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2017-11-21 14:16:34 -0500

    html content type

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 var server = http.createServer(function(request, response) {
 
-  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.writeHead(200, {"Content-Type": "text/html"});
   response.end("<h1>Hello World!</h1>\n");
   console.log("GET /");
 
